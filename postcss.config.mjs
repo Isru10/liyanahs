bfdebd823c83eba47d399dd69e967c081d7607ef
Isru10 <149589109+Isru10@users.noreply.github.com>
2026-06-05 23:39:53 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,4 @@ import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
-
-
 const config = { plugins: { "@tailwindcss/postcss": {}, }, };  export default config;
-
